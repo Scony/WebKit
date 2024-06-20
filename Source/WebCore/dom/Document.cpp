2deca9de55d1f4781c857af9fd8003f37f2cf72c
Pawel Lampe <plampe@igalia.com>
2024-06-20 13:40:20 +0200

    Fallback to full-layout happens when more than 1 subtree layout is pending https://bugs.webkit.org/show_bug.cgi?id=275394
    Reviewed by NOBODY (OOPS!).
    
    Originally, subtree layout was designed to work with a single subtree. However,
    there are use-cases which require subtree layout to support multiple subtrees at once
    so that no fallback to "full" layout is done.
    
    One of such use-cases is CSS containment, which (under certain conditions)
    allows one to isolate parts of DOM tree so that in case of internal changes they can
    be laid out without layouting full tree. With CSS containment, it's fairly common that
    such internal subtree changes will happen at once thus leading to multiple subtree layouts
    being scheduled. Unfortunately, currenly such situation leads to fallback to "full" layout
    as only single subtree layout is supported. Fortunately, currently it's possible (although
    not ideal) to workaround that problem by forcing layout after each subtree change using
    position queries such as document.body.offsetLeft etc.
    
    This PR adds support for layouting multiple subtrees at once. This is achived
    by collecting pending subtrees and eventually layouting them one by one.

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -3278,7 +3278,7 @@ bool Document::updateLayoutIfDimensionsOutOfDate(Element& element, OptionSet<Dim
                 }
             }
 
-            if (currentRenderer == frameView->layoutContext().subtreeLayoutRoot())
+            if (frameView->layoutContext().hasSubtreeLayoutRoot(*currentRenderer))
                 break;
         }
     }
